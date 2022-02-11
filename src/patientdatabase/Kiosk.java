f8bc5f6671449df1f8b9a22ba7a94d2885e46dcb
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-02-11 18:23:47 -0500

    created basic outline for kiosk.java

--- src/patientdatabase/Kiosk.java
+++ src/patientdatabase/Kiosk.java
@@ -1,6 +1,100 @@
 package patientdatabase;
 
+import java.util.Scanner;
+
 public class Kiosk {
+
+    public void run() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Kiosk running. Ready to process transactions.");
+
+        String commandInput = input.nextLine().toUpperCase();
+
+        while (!commandInput.equals("Q")){
+
+
+
+            switch (commandInput){
+
+                case "C": commandInput = "C";
+
+                System.out.println("command C chosen");
+                //cancel appt and remove specified appt from schedule
+                    //EX: C 8/31/1978 Jane Doe 7/19/2022 9:15 middlesex
+                break;
+
+                case "CP": commandInput = "CP";
+                    System.out.println("command CP chosen");
+
+                    //cancel all appts of a given patient & remove all appts for this appt from the shedule
+
+                    //EX: CP 8/31/1978 Jane Doe
+
+                break;
+
+                case "P": commandInput = "P";
+
+                    System.out.println("command P chosen");
+
+                    //display all appts from schedule to console with the current order in the array
+
+
+                break;
+
+                case "PZ": commandInput = "PZ";
+                    System.out.println("command PZ chosen");
+
+                    //display all appts in the schedule ordered by zipcode
+                    //if two appts have same zipcode display the appt with the earlier timeslot first
+
+                break;
+
+                case "PP": commandInput = "PP";
+
+                    System.out.println("command PP chosen");
+
+
+                    //display all apts in the schedule ordered by patients
+                    //sort by patients last name, then first names then date of birth
+
+                    //for patients with multiple appointments, display appts based on their current order in the array
+                    //the order of hte appointments for the same patient doesnt matter)
+
+                break;
+                    //stops program execution and display 'kiosk session ended'
+                case "Q": commandInput = "Q";
+
+                    System.out.println("command Q chosen");
+
+
+
+                break;
+            }
+
+            commandInput = input.nextLine().toLowerCase();
+
+
+
+
+        }
+        System.out.println("Kiosk session ended");
+        System.exit(0);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 
     //this class is the user interface class to process the transactions entered through the console
     //and output the results to the console
